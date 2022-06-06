d446ce97d63887a87cb775f52b38b7889e2c9ae3
Masum Alam <masum.7788alam@gmail.com>
2022-06-06 12:18:32 +0530

    no change

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+$(function () {
+
+scrollBtn.onclick = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    
+    })
+}   
+    
+
+    $("#naviBar").click(function () {
+        $("#navOpen").slideToggle(1500);
+    })
+    $("#navClose").click(function () {
+        $("#navOpen").fadeOut(300);
+    })
+
+    /*skill metter
+    
+    $(window).scroll(function () {
+        $('.skill').each(function () {
+            $(this).find('.progrss').animate({
+                width: $(this).attr('data-percent'),
+            }, 6000);
+        });
+    })*/
+    
+ });
+    
+
+    
+    
